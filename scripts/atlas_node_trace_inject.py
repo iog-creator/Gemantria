fccb93120f42be3282b349796b5b20c87b5d607f
Bryon McCoy <mccoyb00@gmail.com>
2025-11-12 09:32:59 -0800

    style: apply ruff formatting (pre-commit)

--- scripts/atlas_node_trace_inject.py
+++ scripts/atlas_node_trace_inject.py
@@ -16,7 +16,7 @@ if "data-trace-id=" not in txt:
     if "<div class='node' id='n0'" in txt:
         txt = txt.replace(
             "<div class='node' id='n0'",
-            f'<div class=\'node\' id=\'n0\' data-trace-id="{trace}" trace-link="true"',
+            f"<div class='node' id='n0' data-trace-id=\"{trace}\" trace-link=\"true\"",
         )
     elif "<html" in txt and "data-trace-id=" not in txt:
         txt = txt.replace("<html", f'<html data-trace-id="{trace}" trace-link="true"', 1)
